a712bbebf12e9832c81475f1e4330d3794b70dd6
iAmAndrewCarroll <athomascarroll@gmail.com>
2023-12-19 11:54:42 -0800

    vm script

--- scripts/vmtest.ps1
+++ scripts/vmtest.ps1
@@ -1,13 +1,20 @@
 # Function to display existing input with option to edit
 function ShowAndEditInput($variableName) {
-  if ([PSCustomObject]::ContainsKey($PSBoundParameters, $variableName)) {
+  if ($PSBoundParameters.ContainsKey($variableName)) {
     $existingValue = ${$variableName}
-    Write-Host "Current value for $variableName: $existingValue"
-    Write-Host "Press Enter to keep, or type your new value and press Enter:"
+    Write-Host "Current value for ${variableName}: ${existingValue}"
+    Write-Host "Press Enter to keep, or enter a new value and press Enter:"
     $newValue = Read-Host ""
     if ($newValue) {
       ${$variableName} = $newValue
       Write-Host "Value updated."
+    }
+  } else {
+    Write-Host "Enter the value for " + $variableName + ":"
+    $newValue = Read-Host "Enter the path to the ISO image file (.iso)"
+    if ($newValue) {
+      ${$variableName} = $newValue
+      Write-Host "Value saved."
     }
   }
 }
@@ -24,59 +31,42 @@ $Username = ""
 $Password = ""
 
 # Main program loop
-while (!$VMName) {
-  Write-Host "Enter the name for the virtual machine:"
-  $VMName = Read-Host ""
-  ShowAndEditInput "VMName"
-}
+ShowAndEditInput "VMName"
 
 while (!$ISOpath) {
-  Write-Host "Enter the path to the Windows 10 ISO image:"
-  $ISOpath = Read-Host ""
   ShowAndEditInput "ISOpath"
 }
 
-while (-not ([int]$Generation -like "2|3")) {
-  Write-Host "Enter the virtual machine generation (2 or 3):"
-  $Generation = Read-Host ""
+# Validate Generation with input limit
+$invalidCount = 0
+while (-not ([int]$Generation -like "2|3") -and $invalidCount -lt 3) {
   ShowAndEditInput "Generation"
+  $invalidCount++
 }
 
+if ($invalidCount -ge 3) {
+  Write-Error "Invalid generation input entered too many times. Exiting..."
+  exit
+}
+
+# Validate Memory with minimum value
 while (-not ([int]$MemoryStartupBytes -ge 1024)) {
-  Write-Host "Enter the RAM allocated to the VM (in MB, minimum 1024):"
-  $MemoryStartupBytes = Read-Host ""
   ShowAndEditInput "MemoryStartupBytes"
 }
 
+# Validate Processor count with minimum value
 while (-not ([int]$ProcessorCount -ge 1)) {
-  Write-Host "Enter the number of CPU cores for the VM (minimum 1):"
-  $ProcessorCount = Read-Host ""
   ShowAndEditInput "ProcessorCount"
 }
 
-while (!$VHDPath) {
-  Write-Host "Enter the path for the virtual hard disk file (VHDX):"
-  $VHDPath = Read-Host ""
-  ShowAndEditInput "VHDPath"
-}
+ShowAndEditInput "VHDPath"
 
-while (!$NetworkAdapterName) {
-  Write-Host "Enter the name of the network adapter to connect to:"
-  $NetworkAdapterName = Read-Host ""
-  ShowAndEditInput "NetworkAdapterName"
-}
+ShowAndEditInput "NetworkAdapterName"
 
-while (!$Username) {
-  Write-Host "Enter the administrator username for the VM:"
-  $Username = Read-Host ""
-  ShowAndEditInput "Username"
-}
+ShowAndEditInput "Username"
 
-while (!$Password) {
-  Write-Host "Enter the password for the administrator user (hidden):"
-  $Password = Read-Host -AsSecureString
-  ShowAndEditInput "Password"
-}
+# Hide password input
+$Password = Read-Host -AsSecureString "Enter the password for the administrator user (hidden):"
 
 # Display summary before proceeding
 Write-Host "---------- Summary ----------"
@@ -93,14 +83,27 @@ Write-Host "---------- Continue? (Y/N) ----------"
 $confirmation = Read-Host ""
 if ($confirmation -eq "Y") {
 
-  # Script logic to set up the VM using entered information
-  # (replace this with your actual VM creation code)
+  # Import-Module PS-VirtualBox
+  Import-Module PS-VirtualBox
 
-  Write-Host "Setting up virtual machine..."
-  # ... your VM creation code here ...
+  # Create a new VM
+  $vm = New-VM -Name $VMName -MemoryStartupBytes $MemoryStartupBytes -ProcessorCount $ProcessorCount -Generation $Generation
 
-  Write-Host "Virtual machine setup complete!"
+  # Add virtual hard disk and CD/DVD drive
+  Set-VMHardDiskDrive -VM $vm -Path $VHDPath -ControllerIDE 0
+  Set-VMHardDiskDrive -VM $vm -Path $ISOpath -ControllerIDE 1 -MediaType CDDVD
+
+  # Configure network adapter (adjust based on desired type)
+  New-VMNetworkAdapter -VM $vm -Name $NetworkAdapterName -AdapterType NAT
+
+  # Set VM username and password
+  Set-VMGuestProperty -VM $vm -Name "AutoLogonEnabled" -Value 1
+  Set-VMGuestProperty -VM $vm -Name "AutoLogonUser" -Value $Username
+  Set-VMGuestProperty -VM $vm -Name "AutoLogonPassword" -Value $Password
+
+  # Start the VM
+  Start-VM -VM $vm
+  Write-Host "Virtual machine started. Name: $vm.Name"
 } else {
-  Write-Host "Setup cancelled."
+  Write-Host "Virtual machine creation cancelled."
 }
-
